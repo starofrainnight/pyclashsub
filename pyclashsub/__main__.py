0639bffdf6ac4d33334ccad8704910a978ffc9ab
Hong-She Liang <sorn@taorules.com>
2022-09-25 23:04:28 +0800

    Fixed output config file escaped unicode text
    Now we will keep the unicode text and ensure output UTF-8 encoding config file

--- pyclashsub/__main__.py
+++ pyclashsub/__main__.py
@@ -14,64 +14,97 @@ import ruamel.yaml as yaml
 import datetime
 import shutil
 import tempfile
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
+
 
 @click.command()
-@click.option("-c", "--config-dir", required=True, help="Clash's config directory")
-@click.option("-u", "--url", required=True, help="Subscribe url from your provider")
-@click.option('--no-backup', is_flag=True, help="Don't backup original config before update")
+@click.option(
+    "-c", "--config-dir", required=True, help="Clash's config directory"
+)
+@click.option(
+    "-u", "--url", required=True, help="Subscribe url from your provider"
+)
+@click.option(
+    "--no-backup",
+    is_flag=True,
+    help="Don't backup original config before update",
+)
 def main(config_dir, url, no_backup):
     """Clash subscriber"""
     trojan_proxy_prefix = "pyclashsub-trojan-"
     trojan_proxy_name_pattern = "%s%%s" % trojan_proxy_prefix
 
     cfg_path = os.path.join(config_dir, "config.yaml")
-    with open(cfg_path, 'r') as f:
+    with open(cfg_path, "r") as f:
         cfg = yaml.load(f, yaml.RoundTripLoader)
 
     resp = requests.get(url)
-    data = base64.b64decode(resp.content).decode('utf-8')
+    data = base64.b64decode(resp.content).decode("utf-8")
 
     # Clear all auto injected trojan proxies
     proxies = cfg["proxies"]
     if proxies is None:
         proxies = list()
-    proxies = list(filter(lambda it: not it['name'].startswith(trojan_proxy_prefix), proxies))
+    proxies = list(
+        filter(
+            lambda it: not it["name"].startswith(trojan_proxy_prefix), proxies
+        )
+    )
     cfg["proxies"] = proxies
 
-    for i, line in enumerate(data.splitlines()):
-        print("%s: Found proxy %s" % (i, line))
+    only_includes = cfg["pyclashsub"]["only-includes"]
+
+    i = -1
+    for proxy_idx, line in enumerate(data.splitlines()):
+
+        print("%s: Found proxy %s" % (proxy_idx, line))
 
         result = urlparse(line)
-        if 'trojan' != result.scheme:
+
+        if "trojan" != result.scheme:
             continue
 
+        node_text = unquote(result.fragment).strip()
+        if node_text not in only_includes:
+            continue
+
+        i += 1
+
         aproxy = dict()
-        aproxy['type'] = 'trojan'
-        aproxy['name'] = trojan_proxy_name_pattern % i
-        aproxy['server'] = result.hostname
-        aproxy['port'] = result.port
-        aproxy['password'] = result.username
+        aproxy["type"] = "trojan"
+        aproxy["name"] = trojan_proxy_name_pattern % i
+        aproxy["server"] = result.hostname
+        aproxy["port"] = result.port
+        aproxy["password"] = result.username
 
         qs = urllib.parse.parse_qs(result.query)
-        qs_value = qs.get('allowInsecure', list())
+        qs_value = qs.get("allowInsecure", list())
         if (len(qs_value) > 0) and bool(int(qs_value[0])):
-            aproxy['skip-cert-verify']= True
+            aproxy["skip-cert-verify"] = True
 
-        qs_value = qs.get('sni', list())
+        qs_value = qs.get("sni", list())
         if len(qs_value) > 0:
-            aproxy['sni'] = qs_value[0]
+            aproxy["sni"] = qs_value[0]
 
         proxies.append(aproxy)
 
     if not no_backup:
         # Backup original config file
-        shutil.copy2(cfg_path, "%s.%s" % (cfg_path, datetime.datetime.today().isoformat()))
+        shutil.copy2(
+            cfg_path,
+            "%s.%s" % (cfg_path, datetime.datetime.today().isoformat()),
+        )
     cfg_mode = os.stat(cfg_path).st_mode
 
     # Dump to temporary file
     temp_cfg_file = tempfile.NamedTemporaryFile("w+")
-    yaml.dump(cfg, temp_cfg_file, Dumper=yaml.RoundTripDumper)
+    yaml.dump(
+        cfg,
+        temp_cfg_file,
+        Dumper=yaml.RoundTripDumper,
+        encoding="utf-8",
+        allow_unicode=True,
+    )
 
     # Overwrite the cfg file by generated temporary file
     shutil.copy2(temp_cfg_file.name, cfg_path)
